68a58a6c072e1bb53cd607098a79cb26c7861d38
SalamanderXVIII <andreysky@list.ru>
2023-09-23 16:38:21 +0200

    Code fix.
    Script from previous commit now actually works

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,39 +1,58 @@
 using UnityEngine;
 using Cinemachine;
 using UnityEngine.U2D;
+using Unity.VisualScripting;
 
 namespace Scene
 {
 	public class CameraController : MonoBehaviour
 	{
-		[Header("Привязка компонентов")]
-		//[SerializeField] private CinemachineBrain cameraBrain;
-		[Tooltip("Виртуальная камера")]
+		[Header("Components")]
+		[Tooltip("Virtual Camera. It is an object that contains/will contain CinemachineVirtualCamera. Uses game object that script is attached to or it can be asigned manually")]
 		public GameObject virtualCamera;
 
-		[Header("Параметры камеры")]
-		[Tooltip("Плавность следования камеры. Больше = плавнее")]
-		public float cameraSmoothness;
-		[Tooltip("Разрешение камеры")]
-		[SerializeField] private int xResolution;
-		[SerializeField] private int yResolution;
+		[Header("Camera")]
+		[Tooltip("Camera smoothness with which it moves. Bigger number = smoother")]
+		public float cameraSmoothness = 1;
+		[Tooltip("Camera X resolution")]
+		public int xResolution = 1280;
+		[Tooltip("Camera Y resolution")]
+		public int yResolution = 720;
+		[Tooltip("Camera's ortho size. Changes overall camera size")]
+		public float cameraOrthoSize = 3f;
 
-		[Header("Прочие параметры")]
-		[Tooltip("Transform компонент объекта игрока")]
+		[Header("Misc")]
+		[Tooltip("Transform component of player object")]
 		public Transform playerTransform;
-		[Tooltip("Границы камеры, за которые она не сможет зайти")]
+		[Tooltip("Camera borders which camera is not supposed to cross.")]
 		public PolygonCollider2D polygonCollider;
+
+		private Vector2 screenBounds;
+		private float objectWidth;
+		private float objectHeight;
 
 		private void Awake()
 		{
 			CinemachineComponentsInitialisation();
-			CinemachineInitialisation();
+			CinemachineVirtualValues();
 		}
 
-		private void CinemachineInitialisation()
+		private void Start()
+		{
+			//boundValuesSet();
+		}
+
+		private void Update()
+		{
+			//keepingPlayerInBounds();
+		}
+
+		//Pastes values asigned in script to it's respective parameters in CinemachineVirtualCamera and main Camera.
+		public void CinemachineVirtualValues()
 		{
 			virtualCamera.GetComponent<CinemachineVirtualCamera>().m_Follow = playerTransform;
 			virtualCamera.GetComponent<CinemachineVirtualCamera>().GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = polygonCollider;
+			virtualCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = cameraOrthoSize;
 			PixelPerfectCamera mainPixelResolution = Camera.main.GetComponent<PixelPerfectCamera>();
 			mainPixelResolution.refResolutionX = xResolution;
 			mainPixelResolution.refResolutionY = yResolution;
@@ -43,27 +62,83 @@ namespace Scene
 			transposer.m_ZDamping = cameraSmoothness;
 		}
 
+		//Checks for all of the necessary components and adds them if absent.
 		private void CinemachineComponentsInitialisation()
 		{
+			if (virtualCamera == null)
+			{
+				virtualCamera = gameObject;
+			}
+
 			Camera.main.gameObject.TryGetComponent<CinemachineBrain>(out var brain);
 			if (brain == null)
 			{
 				brain = Camera.main.gameObject.AddComponent<CinemachineBrain>();
 
-				Debug.Log("Инициализирован CinemachineBrain.");
+				Debug.Log("CinemachineBrain initialized.");
 			}
-			else
-				Debug.Log("CinemachineBrain не требует инициализации.");
+
+			Camera.main.gameObject.TryGetComponent<PixelPerfectCamera>(out var pixelCamera);
+			if (pixelCamera == null)
+			{
+				pixelCamera = Camera.main.gameObject.AddComponent<PixelPerfectCamera>();
+
+				Debug.Log("PixelPerfectCamera initialized");
+			}
 
 			virtualCamera.TryGetComponent<CinemachineVirtualCamera>(out var virtCam);
 			if (virtCam == null)
 			{
 				virtCam = virtualCamera.AddComponent<CinemachineVirtualCamera>();
+				virtCam.AddCinemachineComponent<CinemachineTransposer>();
+				virtCam.AddComponent<CinemachineConfiner2D>();
+				virtCam.AddComponent<Cinemachine.CinemachinePixelPerfect>();
 
-				Debug.Log("Инициализирован CinemachineVirtualCamera");
+				Debug.Log("CinemachineVirtualCamera initialized");
 			}
 			else
-				Debug.Log("CinemachineVirtualCamera не требует инициализации.");
+			{
+				virtualCamera.TryGetComponent<CinemachineConfiner2D>(out var conf);
+				if (conf == null)
+				{
+					conf = virtualCamera.AddComponent<CinemachineConfiner2D>();
+
+					Debug.Log("CinemachineConfiner2D initialized");
+				}
+
+				virtualCamera.TryGetComponent<CinemachineTransposer>(out var trans);
+				if (trans == null)
+				{
+					trans = virtualCamera.AddComponent<CinemachineTransposer>();
+
+					Debug.Log("CinemachineTransposer initialized");
+				}
+
+				virtualCamera.TryGetComponent<Cinemachine.CinemachinePixelPerfect>(out var pixelVirtualCheck);
+				if (pixelVirtualCheck == null)
+				{
+					pixelVirtualCheck = virtualCamera.AddComponent<Cinemachine.CinemachinePixelPerfect>();
+
+					Debug.Log("CinemachineTransposer initialized");
+				}
+			}
+		}
+
+		//Sets camera bounds.
+		private void boundValuesSet()
+		{
+			objectWidth = playerTransform.GetComponent<BoxCollider2D>().bounds.size.x / 2;
+			objectHeight = playerTransform.GetComponent<BoxCollider2D>().bounds.size.y / 2;
+		}
+
+		//Keeps player within camera bounds.
+		private void keepingPlayerInBounds()
+		{
+			screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+			Vector3 viewPos = playerTransform.position;
+			viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 - objectWidth, screenBounds.x + objectWidth);
+			viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 - objectHeight, screenBounds.y + objectHeight);
+			playerTransform.position = viewPos;
 		}
 	}
 }
